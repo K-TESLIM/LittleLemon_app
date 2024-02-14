80d986f775ab4c5707f6c3333f175e3d9a4385ca
K-TESLIM <kamaldeenteslim@gmail.com>
2024-02-14 14:27:55 +0100

    I do not want it to cover the height of the screen, so I change the modifier from fillmaxsize to fillmaxwidth. When I run it, the UI now covers the upper half of the screen with all the modifiers added.

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -8,16 +8,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -43,7 +44,7 @@ fun MainComponent() {
 
     Column(
         Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
             .background(Color(0XFFF495E57)),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start
@@ -62,12 +63,17 @@ fun MainComponent() {
         Row(
             Modifier
                 .fillMaxWidth()
-                .padding(20.dp), horizontalArrangement = Arrangement.Center) {
+                .padding(20.dp), horizontalArrangement = Arrangement.Start) {
+            Text(text = stringResource(id = R.string.descriptionone),
+                Modifier.width(200.dp),
+                color = Color.White,
+                fontSize = 21.sp)
 
             Image(
                 painter = painterResource(id = R.drawable.restaurantfoodb),
                 contentDescription = "",
-                Modifier.height(100.dp)
+                Modifier.height(200.dp).clip((RoundedCornerShape(20.dp))
+                )
             )
         }
         Button(
